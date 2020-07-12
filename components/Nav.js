00c85a408cbd33e09246342d04c4e1a8a22ea95e
shane.mccoy <shane.mccoy@nwea.org>
2020-07-12 11:24:35 -0700

    update...

--- components/Nav.js
+++ components/Nav.js
@@ -21,7 +21,7 @@ const Nav = () => {
         </li>
         <li>
           <Link href='/habits'>
-            <a>Habit</a>
+            <a>Habits</a>
           </Link>
         </li>
       </ul>
